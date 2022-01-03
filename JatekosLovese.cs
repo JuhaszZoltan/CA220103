459952fa8a02c1788de1bc18e5caf00cffc9f6a9
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2022-01-03 19:58:11 +0100

    fin

---
+++ JatekosLovese.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA220103
+{
+    internal class JatekosLovese
+    {
+        private static (float X, float Y) _celtabla;
+        private static bool _isCeltablaSet = false;
+        public static (float X, float Y) Celtabla
+        {
+            set
+            {
+                if (_isCeltablaSet) throw new Exception("futás közben ne baszogasd!");
+                _celtabla = value;
+                _isCeltablaSet = true;
+            }
+            get => _celtabla;
+        }
+        public int Sorszam { get; set; }
+        public string Nev { get; set; }
+        public (float X, float Y) Loves { get; set; }
+
+        public float Tavolsag => (float)Math.Sqrt(
+            Math.Pow(Celtabla.X - Loves.X, 2) +
+            Math.Pow(Celtabla.Y - Loves.Y, 2));
+
+        public float Pontszam =>
+            (10 - Tavolsag) > 0 ? (float)Math.Round(10 - Tavolsag, 2) : 0F;
+
+        public JatekosLovese(int sorszam, string fileSora)
+        {
+            Sorszam = sorszam;
+            var v = fileSora.Split(';');
+            Nev = v[0];
+            Loves = (
+                X: float.Parse(v[1]),
+                Y: float.Parse(v[2]));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,71 @@
-﻿
+﻿using CA220103;
+using System.Text;
+
+List<JatekosLovese> lista = new();
+
+Feladat04();
+Feladat05();
+Feladat07();
+Feladat09();
+Feladat10();
+Feladat11_12_13();
+
+
+void Feladat11_12_13()
+{
+    var jl = lista.GroupBy(x => x.Nev);
+    Console.WriteLine("11. feladat: Lövések száma:");
+    foreach (var j in jl)
+    {
+        Console.WriteLine($"\t{j.Key} - {j.Count()} db");
+    }
+
+    Console.WriteLine("12. feladat: Átlagpontszámok:");
+    (string Nev, float Atlag) gyoztes = ("", 0F);
+    foreach (var j in jl)
+    {
+        var jn = j.Key;
+        var jap = j.Average(x => x.Pontszam);
+        if (jap > gyoztes.Atlag) gyoztes = (jn, jap);
+        Console.WriteLine($"\t{jn} - {jap} db");
+    }
+    Console.WriteLine($"13. feladat: A játék nyertese: {gyoztes.Nev}");
+}
+void Feladat10()
+{
+    int jsz = lista.DistinctBy(x => x.Nev).Count();
+    Console.WriteLine($"10. feladat: Játékosok száma: {jsz}");
+}
+void Feladat09()
+{
+    int np = lista.Count(x => x.Pontszam == 0);
+    Console.WriteLine($"9. feladat: Nulla pontos lövések száma: {np} db");
+}
+void Feladat04()
+{
+
+    using StreamReader sr = new(
+        "..\\..\\..\\Resources\\lovesek.txt",
+        Encoding.UTF8);
+
+    var es = sr.ReadLine().Split(';');
+
+    JatekosLovese.Celtabla = (float.Parse(es[0]), float.Parse(es[1]));
+
+    int ssz = 1;
+    while (!sr.EndOfStream)
+    {
+        lista.Add(new JatekosLovese(ssz, sr.ReadLine()));
+        ssz++;
+    }
+}
+void Feladat05()
+{
+    Console.WriteLine($"5. feladat: Lövések száma: {lista.Count} db");
+}
+void Feladat07()
+{
+    var gy = lista.Where(x => x.Tavolsag == lista.Min(y => y.Tavolsag)).First();
+    Console.WriteLine("7. feladat: Legpontosabb lövés: \n\t" +
+        $"{gy.Sorszam}.; {gy.Nev}; x={gy.Loves.X}; y={gy.Loves.Y}; távolság:{gy.Tavolsag}");
+}
